86c5c83bafb448b5d6b3de6601a97be089931864
oleksandrlysachenko <lysachenko.oleksandr@gmail.com>
2015-07-23 18:52:25 +0300

    add new function

--- test.js
+++ test.js
@@ -1,4 +1,7 @@
 function randomRange(min,max){
+    min = min || 1;
+    max = max ||100;
     return Math.floor(Math.random()*(max-min)+min);
 }
 console.log(randomRange(1,20));
+console.log(randomRange(1));
